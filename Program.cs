6ee50c099b163abb1dfe10493e92b891ea28ac8c
luismi-90 <114842181+luismi-90@users.noreply.github.com>
2022-10-02 01:02:07 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿int f, c;
+string n;
+Console.Write("Ingrese numero de COLUMNAS: ");
+f = int.Parse(Console.ReadLine());
+Console.Write("Ingrese numero de FILAS: ");
+c = int.Parse(Console.ReadLine());
+Console.Write("Ingrese el numero a mostar: ");
+n = (Console.ReadLine());
+for (int i = 0; i < c; i++)
+{
+    for (int j = 0; j < f; j++)
+    {
+        Console.Write("" + n + "");
+    }
+    Console.Write("\n");
+}
