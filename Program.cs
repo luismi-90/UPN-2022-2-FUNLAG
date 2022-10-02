642cd63835f77a6468eca7dff944c28bfdd257d3
luismi-90 <114842181+luismi-90@users.noreply.github.com>
2022-10-02 00:58:22 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+bool esPrimo;
+int n = 100;
+string primos = "";
+for (int x = 2; x <= n; x++)
+{
+    esPrimo = true;
+    for (int y = 2; y < x; y++)
+    {
+        if (x % y == 0)
+        {
+            esPrimo = false;
+            break;
+        }
+    }
+    if (esPrimo) primos += x + "-";
+}
+Console.WriteLine("Los numeros primos son: " + primos);
+Console.ReadKey();
