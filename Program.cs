bbef0ac7bbcaa9235aeb97de1e09e4ad5121b318
luismi-90 <114842181+luismi-90@users.noreply.github.com>
2022-10-02 01:05:43 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿char caracter;
+do
+{
+    Console.Write("INGRESE UNA LETRA: ");
+    caracter = char.Parse(Console.ReadLine());
+}
+while (caracter != 'a' && caracter != 'e' && caracter != 'i' && caracter != 'o' && caracter != 'u' && caracter != 'A' && caracter != 'E' && caracter != 'I' && caracter != 'O' && caracter != 'U');
+Console.Write("LA PRIMERA VOCAL INGRESADA ES : "+caracter);
